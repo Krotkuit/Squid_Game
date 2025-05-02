e607bc3ae8b1f0cfe5720e11a95384c4a1b9b146
Krotkuit <bouchel.baptiste@gmail.com>
2025-05-02 03:32:59 +0200

    Modifs PRV ajoute la possibilité l'exécution par command block

--- src/main/java/fr/salut/squidgame/component/commands/games/PRVCommand.java
+++ src/main/java/fr/salut/squidgame/component/commands/games/PRVCommand.java
@@ -3,7 +3,7 @@ package fr.salut.squidgame.component.commands.games;
 import fr.salut.squidgame.SquidGame;
 import fr.salut.squidgame.component.ListenerManager.MiniGames.PRV.PRVListener;
 import fr.salut.squidgame.component.ListenerManager.MiniGames.PRV.PRVState;
-import org.bukkit.entity.Player;
+import org.bukkit.command.CommandSender;
 import revxrsal.commands.annotation.Command;
 import revxrsal.commands.annotation.DefaultFor;
 import revxrsal.commands.annotation.Subcommand;
@@ -14,25 +14,25 @@ public class PRVCommand {
     SquidGame plugin = SquidGame.getInstance();
 
     @DefaultFor("~")
-    public void bapDefault(Player sender){
+    public void bapDefault(CommandSender sender){
         sender.sendMessage("§cÉtat invalide. Utilisez ON, OFF ou STOP.");
     }
 
     @Subcommand("ON")
-    public void prvON(Player sender){
+    public void prvON(CommandSender sender){
         plugin.setPrvState(PRVState.ON);
         sender.sendMessage("§aPRV activé : toutes les procédures fonctionnent.");
     }
 
     @Subcommand("OFF")
-    public void prvOFF(Player sender){
+    public void prvOFF(CommandSender sender){
         plugin.setPrvState(PRVState.OFF);
         PRVListener.resetPrisoners();
         sender.sendMessage("§cPRV désactivé : aucune procédure ne fonctionne.");
     }
 
     @Subcommand("STOP")
-    public void prvSTOP(Player sender){
+    public void prvSTOP(CommandSender sender){
         plugin.setPrvState(PRVState.STOP);
         sender.sendMessage("§ePRV en mode STOP : seules les prisons fonctionnent.");
     }
