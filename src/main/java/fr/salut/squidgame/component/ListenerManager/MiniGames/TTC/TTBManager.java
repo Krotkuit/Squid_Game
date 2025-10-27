29a4b8d02f00afd7c546ceaba71afbe4536b21ba
Nocolm <nocolm77@gmail.com>
2025-10-27 15:18:40 +0100

    fix TTB + add sound and title

--- src/main/java/fr/salut/squidgame/component/ListenerManager/MiniGames/TTC/TTBManager.java
+++ src/main/java/fr/salut/squidgame/component/ListenerManager/MiniGames/TTC/TTBManager.java
@@ -1,6 +1,8 @@
 package fr.salut.squidgame.component.ListenerManager.MiniGames.TTC;
 
 import fr.salut.squidgame.SquidGame;
+import fr.salut.squidgame.component.ListenerManager.intance.TeamManager;
+import fr.salut.squidgame.component.commands.EpreuveCommand;
 import fr.salut.squidgame.component.commands.games.TTBCommand;
 import fr.salut.squidgame.utils.chronometer.Chronometer;
 import fr.salut.squidgame.utils.chronometer.ChronometerType;
@@ -12,12 +14,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
-import org.bukkit.util.RayTraceResult;
-import org.bukkit.util.Vector;
 
 import java.util.*;
 
@@ -33,6 +33,7 @@ public class TTBManager implements Listener {
 
     private static final List<String> allowedTeams = List.of("dark_red", "rouge", "orange", "jaune", "vert_clair", "vert", "vert_profond",
             "bleu", "cyan", "bleu_marine", "bleu_profond", "violet", "magenta", "rose", "blanc", "light_gray", "gris", "dark_gray", "black", "marron");
+    private final Map<Team, BukkitRunnable> activeTask = new HashMap<>();
 
     private final Sound exploseSound = Sound.ENTITY_GENERIC_EXPLODE;
     private final Sound tickSound = Sound.BLOCK_NOTE_BLOCK_HAT;
@@ -41,7 +42,10 @@ public class TTBManager implements Listener {
         teams.clear();
 
         for (Team team : scoreboard.getTeams()){
-            if (allowedTeams.contains(team.getName()) && !team.getEntries().isEmpty()) teams.add(team);
+
+            if (allowedTeams.contains(team.getName()) && TeamManager.hasTeamOnlinePlayers(team)){
+                teams.add(team);
+            }
         }
     }
 
@@ -53,19 +57,32 @@ public class TTBManager implements Listener {
                 bomber.sendActionBar("Vous n'avez plus la bombe");
                 bomber.getInventory().setHelmet(new ItemStack(Material.AIR));
             }
-            bombers.replace(team, target.getUniqueId());
+            if (bombers.containsKey(team)) bombers.replace(team, target.getUniqueId());
+            else bombers.put(team, target.getUniqueId());
         }
+    }
+
+    public static void stopGame(){
+        for (UUID uuid : bombers.values()){
+            Bukkit.getPlayer(uuid).getInventory().remove(Material.TNT);
+        }
+        bombers.clear();
     }
 
     @EventHandler
     public void onServerChronometerEnd(Chronometer.ServerChronometerEndEvent event){
+        if (!EpreuveCommand.getEpreuve().equals("TTB")) return;
         if (allowedTeams.contains(event.getGroup())){
             Team team = scoreboard.getTeam(event.getGroup());
+            Team joueur = scoreboard.getTeam("joueur");
             if (team!=null){
                 Player bomber = Bukkit.getPlayer(bombers.get(team));
+                activeTask.get(team).cancel();
+                activeTask.remove(team);
 
                 if (bomber != null){
                     team.removeEntry(bomber.getName());
+                    joueur.addEntry(bomber.getName());
 
                     bomber.getInventory().setHelmet(new ItemStack(Material.AIR));
                     bomber.getWorld().playSound(bomber.getLocation(), exploseSound, 10, 1);
@@ -73,10 +90,19 @@ public class TTBManager implements Listener {
                     bomber.setHealth(0);
                 }
 
-                if (team.getPlayers().size() > 5 - TTBCommand.maxDead){
-                    UUID newBomber = team.getPlayers().stream().toList().get(TTBCommand.random.nextInt(team.getPlayers().size())).getUniqueId();
+                if (TeamManager.getTeamOnlinePlayers(team).size() > 5 - TTBCommand.maxDead){
+                    UUID newBomber = TeamManager.getTeamOnlinePlayers(team).get(TTBCommand.random.nextInt(TeamManager.getTeamOnlinePlayers(team).size())).getUniqueId();
                     giveBombTo(Bukkit.getPlayer(newBomber), null, team);
+                    for (Player player : TeamManager.getTeamOnlinePlayers(team)) player.sendTitle("§6Next Round", "§c" + Bukkit.getPlayer(newBomber).getName() + " à la bombe !");
                     Chronometer.startServerChronometer(null, team, team.getName(), TTBCommand.random.nextInt(90, 150), ChronometerType.ACTION_BAR, "%null%", ChronometerType.ACTION_BAR, "§cBOUM !");
+                } else {
+                    bombers.remove(team);
+                    for (Player player : TeamManager.getTeamOnlinePlayers(team)) player.sendMessage("§aVeuillez attendre que toutes les équipes aient fini ");
+                    Team gardeTeam = Bukkit.getScoreboardManager().getMainScoreboard().getTeam("garde");
+                    for (Player garde : TeamManager.getTeamOnlinePlayers(gardeTeam)){
+                        garde.sendMessage("§eEquipe "+ team.getName() +"§a à fini");
+                        if (bombers.isEmpty()) garde.sendMessage("§a Toutes les équipes ont fini !");
+                    }
                 }
             }
         }
@@ -84,55 +110,34 @@ public class TTBManager implements Listener {
 
     @EventHandler
     public void onServerChronometerTick(Chronometer.ChronometerTimeChangeEvent event){
+        if (!EpreuveCommand.getEpreuve().equals("TTB")) return;
         if (allowedTeams.contains(event.getGroup())){
             Team team = scoreboard.getTeam(event.getGroup());
             if (team!=null){
-                for (Player player : SquidGame.getInstance().getServer().getOnlinePlayers()) {
-                    if (team.hasPlayer(player)) player.playSound(player, tickSound, 8, 1);
+                if (event.getTime() > 45){
+                    for (Player player : TeamManager.getTeamOnlinePlayers(team)) {
+                        player.playSound(player, tickSound, 8, 1);
+                    }
                 }
+                if (event.getTime() == 45 ) tickSoundFast(team);
+                if (event.getTime() == 5) tickSoundVeryFast(team);
+
                 Player bomber = Bukkit.getPlayer(bombers.get(team));
-                if (bomber != null) bomber.sendActionBar("§cVous avez la bombe !");;
-            }
-        }
-    }
-
-    @EventHandler
-    public void onPlayerGiveBomb(PlayerInteractEvent event) {
-
-        Player player = event.getPlayer();
-
-        Vector direction = player.getLocation().getDirection().normalize();
-
-        RayTraceResult result = player.getWorld().rayTraceEntities(
-                player.getEyeLocation(),
-                direction,
-                1000,
-                entity -> entity instanceof Player && !entity.equals(player)
-        );
-
-        if (result != null && result.getHitEntity() instanceof Player target) {
-            Team team = scoreboard.getPlayerTeam(player);
-            Team targetTeam = scoreboard.getPlayerTeam(target);
-
-            if (targetTeam == null || team == null) return;
-            if (team.equals(targetTeam)){
-                if (bombers.get(team).equals(player.getUniqueId())){
-                    giveBombTo(target, player, team);
-                }
+                if (bomber != null) bomber.sendActionBar("§cVous avez la bombe !");
             }
         }
     }
 
     @EventHandler
     public void onPlayerHitPlayer(EntityDamageByEntityEvent e) {
-
+        if (!EpreuveCommand.getEpreuve().equals("TTB")) return;
         if (e.getDamager() instanceof Player player) {
             if (e.getEntity() instanceof Player target) {
                 Team team = scoreboard.getPlayerTeam(player);
                 Team targetTeam = scoreboard.getPlayerTeam(target);
 
                 if (targetTeam == null || team == null) return;
-                if (team.equals(targetTeam)){
+                if (team.getName().equals(targetTeam.getName())){
                     if (bombers.get(team).equals(player.getUniqueId())){
                         giveBombTo(target, player, team);
                     }
@@ -140,4 +145,30 @@ public class TTBManager implements Listener {
             }
         }
     }
-}
+
+    private void tickSoundFast(Team team){
+        BukkitRunnable task = new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (Player player : TeamManager.getTeamOnlinePlayers(team)) player.playSound(player, tickSound, 8, 1);
+            }
+        };
+        task.runTaskTimer(SquidGame.getInstance(), 0, 10); // Exécute toutes les secondes (20 ticks)
+
+        activeTask.put(team, task);
+    }
+
+    private void tickSoundVeryFast(Team team){
+
+        activeTask.get(team).cancel();
+
+        BukkitRunnable task = new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (Player player : TeamManager.getTeamOnlinePlayers(team)) player.playSound(player, tickSound, 8, 1);
+            }
+        };
+        task.runTaskTimer(SquidGame.getInstance(), 0, 5); // Exécute toutes les secondes (20 ticks)
+
+        activeTask.replace(team, task);
+    }}
--- src/main/java/fr/salut/squidgame/component/commands/games/TTBCommand.java
+++ src/main/java/fr/salut/squidgame/component/commands/games/TTBCommand.java
@@ -1,10 +1,12 @@
 package fr.salut.squidgame.component.commands.games;
 
 import fr.salut.squidgame.component.ListenerManager.MiniGames.TTC.TTBManager;
+import fr.salut.squidgame.component.ListenerManager.intance.TeamManager;
 import fr.salut.squidgame.utils.chronometer.Chronometer;
 import fr.salut.squidgame.utils.chronometer.ChronometerType;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Team;
 import revxrsal.commands.annotation.Command;
 import revxrsal.commands.annotation.Subcommand;
@@ -12,14 +14,14 @@ import revxrsal.commands.annotation.Subcommand;
 import java.util.Random;
 import java.util.UUID;
 
-@Command("ttc")
+@Command("ttb")
 public class TTBCommand {
 
     public static double maxDead = 1;
     public static Random random = new Random();
 
     @Subcommand("set")
-    public void tTCSet(CommandSender sender, double score){
+    public void tTCBet(CommandSender sender, double score){
         if (score <= 0) {
             sender.sendMessage("§cLa valeur doit être positive.");
             return;
@@ -29,17 +31,23 @@ public class TTBCommand {
     }
 
     @Subcommand("start")
-    public void tTTStart(){
+    public void tTBStart(){
 
         TTBManager.addTeams();
 
         for (Team team : TTBManager.getTeams()){
 
-            UUID bomber = team.getPlayers().stream().toList().get(random.nextInt(team.getPlayers().size())).getUniqueId();
+            UUID bomber = TeamManager.getTeamOnlinePlayers(team).get(random.nextInt(TeamManager.getTeamOnlinePlayers(team).size())).getUniqueId();
 
             TTBManager.giveBombTo(Bukkit.getPlayer(bomber), null, team);
-
+            for (Player player : TeamManager.getTeamOnlinePlayers(team)) player.sendTitle("§6Début du jeu", "§c" + Bukkit.getPlayer(bomber).getName() + " à la bombe !");
             Chronometer.startServerChronometer(null, team, team.getName(), random.nextInt(90, 150), ChronometerType.ACTION_BAR, "%null%", ChronometerType.ACTION_BAR, "§cBOUM !");
         }
+    }
+
+    @Subcommand("stop")
+    public void tTBStop() {
+        TTBManager.stopGame();
+        Chronometer.stopAllServerChronometer(null, ChronometerType.ACTION_BAR, "%null%");
     }
 }
